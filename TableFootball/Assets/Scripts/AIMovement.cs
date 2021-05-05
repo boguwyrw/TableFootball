6a786a4cdb52f5debc0b1559dd4d132cb89870da
boguwyrw <b.wyrwal@wp.pl>
2021-05-05 23:26:55 +0200

    Basic AI has been done

--- TableFootball/Assets/Scripts/AIMovement.cs
+++ TableFootball/Assets/Scripts/AIMovement.cs
@@ -14,11 +14,13 @@ public class AIMovement : MonoBehaviour
     private int numberOfMoves;
     private Vector3 AISelectedPosition;
     private List<Vector3> listOfPossibleMoves = new List<Vector3>();
+    private Vector3 AIActualPosition;
 
     private void Start()
     {
         blueLineNumber = 0;
         numberOfMoves = 1;
+        AIActualPosition = Vector3.zero;
 
         CreatingBlueLineRenderer();
 
@@ -44,7 +46,11 @@ public class AIMovement : MonoBehaviour
     private IEnumerator DelayAI_Action()
     {
         yield return new WaitForSeconds(0.6f);
-        PossibleMoves(Ball.ballPositions[Ball.ballPositions.Count - 1]);
+        PossibleMoves(Ball.ballPositions[Ball.ballPositions.Count - 1]); // ETAP 1
+        RemoveForbiddenPositions(listOfPossibleMoves); // ETAP 2
+        RemoveEdgePositions(); // ETAP 3
+        RemovePositionFartherFromGoal(); // ETAP 4
+        CheckNextPossibleMoves(); // ETAP 5
         blueLineRenderer.SetPosition(0, Ball.ballPositions[Ball.ballPositions.Count - 1]);
         blueLineRenderer.SetPosition(1, SelectedPosition());
         ApprovePosition();
@@ -73,16 +79,189 @@ public class AIMovement : MonoBehaviour
                 listOfPossibleMoves.Add(tempPossibleMovesList[i]);
             }
         }
-
-        RemoveForbiddenPositions(listOfPossibleMoves);
+        //Debug.Log("Etap 1: " + listOfPossibleMoves.Count);
+        AIActualPosition = actualPosition;
     }
 
     private void RemoveForbiddenPositions(List<Vector3> positionsList)
     {
-        for (int j = 0; j < positionsList.Count; j++)
+        foreach (List<Vector3> positionIn in MovementController.positionsInOut.ToArray())
         {
-
+            if (positionIn[0] == AIActualPosition)
+            {
+                foreach (Vector3 position in positionsList.ToArray())
+                {
+                    if (position == positionIn[1])
+                        listOfPossibleMoves.Remove(position);
+                }
+            }
         }
+        //Debug.Log("Etap 2: " + listOfPossibleMoves.Count);
+    }
+
+    private void RemoveEdgePositions()
+    {
+        if(AIActualPosition.x == -4)
+        {
+            foreach (Vector3 itemPos in listOfPossibleMoves.ToArray())
+            {
+                if (itemPos.x <= -4)
+                    listOfPossibleMoves.Remove(itemPos);
+            }
+        }
+        else if(AIActualPosition.x == 4)
+        {
+            foreach (Vector3 itemPos in listOfPossibleMoves.ToArray())
+            {
+                if (itemPos.x >= 4)
+                    listOfPossibleMoves.Remove(itemPos);
+            }
+        }
+
+        if (AIActualPosition.y == -5 && AIActualPosition.x < -1 || AIActualPosition.x > 1)
+        {
+            foreach (Vector3 itemPos in listOfPossibleMoves.ToArray())
+            {
+                if (itemPos.y <= -5)
+                    listOfPossibleMoves.Remove(itemPos);
+            }
+        }
+        else if(AIActualPosition.y == 5 && AIActualPosition.x < -1 || AIActualPosition.x > 1)
+        {
+            foreach (Vector3 itemPos in listOfPossibleMoves.ToArray())
+            {
+                if (itemPos.y >= 5)
+                    listOfPossibleMoves.Remove(itemPos);
+            }
+        }
+        //Debug.Log("Etap 3: " + listOfPossibleMoves.Count);
+    }
+
+    private void CheckNextPossibleMoves()
+    {
+        List<List<Vector3>> nextOfPossibleMoves = new List<List<Vector3>>();
+        for (int w = 0; w < listOfPossibleMoves.Count; w++)
+        {
+            nextOfPossibleMoves.Add(new List<Vector3>());
+        }
+        // ETAP 5 - 1
+        for (int k = 0; k < listOfPossibleMoves.Count; k++)
+        {
+            Vector3 nextForbiddenPosition = Ball.ballPositions[Ball.ballPositions.Count - 1];
+            List<Vector3> tempNextPossibleMoves = new List<Vector3>();
+            // liczenie możliwych ruchów zaczyna się od godziny 3 i idzie w przeciwnym kierunku do ruchu wskazówek zegara
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x + 1, listOfPossibleMoves[k].y, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x + 1, listOfPossibleMoves[k].y + 1, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x, listOfPossibleMoves[k].y + 1, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x - 1, listOfPossibleMoves[k].y + 1, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x - 1, listOfPossibleMoves[k].y, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x - 1, listOfPossibleMoves[k].y - 1, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x, listOfPossibleMoves[k].y - 1, 0));
+            tempNextPossibleMoves.Add(new Vector3(listOfPossibleMoves[k].x + 1, listOfPossibleMoves[k].y - 1, 0));
+
+            nextOfPossibleMoves[k] = tempNextPossibleMoves;
+
+            foreach (Vector3 nextPossibleMove in nextOfPossibleMoves[k].ToArray())
+            {
+                if (nextPossibleMove == nextForbiddenPosition)
+                    nextOfPossibleMoves[k].Remove(nextPossibleMove);
+            }
+        }
+
+        // ETAP 5 - 2
+        for (int a = 0; a < listOfPossibleMoves.Count; a++)
+        {
+            foreach (List<Vector3> posInOut in MovementController.positionsInOut.ToArray())
+            {
+                if (posInOut[0] == listOfPossibleMoves[a])
+                {
+                    foreach (Vector3 position in nextOfPossibleMoves[a].ToArray())
+                    {
+                        if (position == posInOut[1])
+                            nextOfPossibleMoves[a].Remove(position);
+                    }
+                }
+            }
+        }
+
+        // ETAP 5 - 3
+        for (int i = 0; i < nextOfPossibleMoves.Count; i++)
+        {
+            if (listOfPossibleMoves[i].x == -4)
+            {
+                foreach (Vector3 itemPos in nextOfPossibleMoves[i].ToArray())
+                {
+                    if (itemPos.x <= -4)
+                        nextOfPossibleMoves[i].Remove(itemPos);
+                }
+            }
+            else if (listOfPossibleMoves[i].x == 4)
+            {
+                foreach (Vector3 itemPos in nextOfPossibleMoves[i].ToArray())
+                {
+                    if (itemPos.x >= 4)
+                        nextOfPossibleMoves[i].Remove(itemPos);
+                }
+            }
+
+            if (listOfPossibleMoves[i].y == -5 && listOfPossibleMoves[i].x < -1 || listOfPossibleMoves[i].x > 1)
+            {
+                foreach (Vector3 itemPos in nextOfPossibleMoves[i].ToArray())
+                {
+                    if (itemPos.y <= -5)
+                        nextOfPossibleMoves[i].Remove(itemPos);
+                }
+            }
+            else if (listOfPossibleMoves[i].y == 5 && listOfPossibleMoves[i].x < -1 || listOfPossibleMoves[i].x > 1)
+            {
+                foreach (Vector3 itemPos in nextOfPossibleMoves[i].ToArray())
+                {
+                    if (itemPos.y >= 5)
+                        nextOfPossibleMoves[i].Remove(itemPos);
+                }
+            }
+        }
+
+        for (int b = 0; b < nextOfPossibleMoves.Count; b++)
+        {
+            //Debug.Log(b + " - nextOfPossibleMoves: " + nextOfPossibleMoves[b].Count);
+            if(nextOfPossibleMoves[b].Count == 0)
+            {
+                listOfPossibleMoves.RemoveAt(b);
+                nextOfPossibleMoves.RemoveAt(b);
+            }
+        }
+        
+        /*
+        foreach (List<Vector3> item in nextOfPossibleMoves.ToArray())
+        {
+            if(item.Count == 0)
+            {
+                nextOfPossibleMoves.Remove(item);
+                listOfPossibleMoves.Remove(item.FindLastIndex(item.Count == 0));
+            }
+        }
+        */
+        Debug.Log("Etap 5: " + listOfPossibleMoves.Count);
+    }
+
+    private void RemovePositionFartherFromGoal()
+    {
+        List<Vector3> tempListOfMoves = new List<Vector3>();
+        bool canMove;
+        for (int i = 0; i < listOfPossibleMoves.Count; i++)
+        {
+            canMove = (-6 - listOfPossibleMoves[i].y) >= (-6 - AIActualPosition.y);
+            if (canMove)
+            {
+                tempListOfMoves.Add(listOfPossibleMoves[i]);
+            }
+        }
+
+        listOfPossibleMoves.Clear();
+        listOfPossibleMoves = tempListOfMoves;
+
+        Debug.Log("Etap 4: " + listOfPossibleMoves.Count);
     }
 
     private Vector3 SelectedPosition()
